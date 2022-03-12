575630052b84e21b78c5b16321ac5891166abf30
Lego Stax <tyrieckmann@protonmail.com>
2022-03-11 21:30:39 -0700

    user.js code is done

--- src/user.js
+++ src/user.js
@@ -12,25 +12,21 @@ const GLOBAL_API_KEY = process.env.API_KEY;
 router.get('/', async (ctx, next) => {
     const userID = await util.verify(ctx);
     if(userID){
-        // TODO: if performance issues, consolidate queries
-        const userInfo = await db.query('select * from users where id = $1', [userID]);
-        // TODO: get interested meatups
+        const userInfo = await db.query('select display_name from users where id = $1', [userID]);
         const userInterests = await db.query('select meatup_id from interested where user_id = $1', [userID]);
-        // TODO: get owned meatups
         const ownedMeatups = await db.query('select * from meatup where owner = $1', [userID]);
-        // TODO: assemble data in JSON object
         const userData = {
-            userInfo.rows[0],
+            name: userInfo.rows[0].display_name,
             interested_meatups: userInterests.rows,
             owned_meatups: ownedMeatups.rows
         };
 
         // ensure it's not empty
-        if(rows.length > 0){
+        if(userInfo.rowCount > 0){
             ctx.response.status = 200;
             ctx.response.body = userData;
         }else{
-            ctx.response.status = 404;
+            ctx.response.status = 400;
         }
     }else{
         ctx.response.status = 404;
@@ -47,14 +43,9 @@ router.post('/', async (ctx, next) => {
         }
         if(('key' in requestBody) && ('name' in requestBody)){
             if(requestBody.key === GLOBAL_API_KEY){
-                // TODO: handle when insert query fails...though I'm not sure it ever would
                 const {rows} = await db.query('insert into users(display_name) values($1) returning id', [requestBody.name]);
                 util.set(ctx, rows[0].id);
                 ctx.response.status = 200;
-                ctx.response.body = {
-                    id: rows[0].id,
-                    name: requestBody.name
-                };
             }else{
                 ctx.response.status = 400;
             }
@@ -63,6 +54,34 @@ router.post('/', async (ctx, next) => {
         }
     }else{
         ctx.response.status = 400;
+    }
+});
+
+// PATCH /api/user
+// update username for current authenticated user
+router.patch('/', async (ctx, next) => {
+    const userID = await util.verify(ctx);
+    if(userID){
+        var requestBody = ctx.request.body;
+        if(requestBody){
+            if(typeof requestBody == 'string'){
+                requestBody = JSON.parse(requestBody);
+            }
+            if('name' in requestBody){
+                const result = await db.query('update users set display_name = $1 where id = $2', [requestBody.name, userID]);
+                if(result.rowCount > 0){
+                    ctx.response.status = 200;
+                }else{
+                    ctx.response.status = 400;
+                }
+            }else{
+                ctx.response.status = 400;
+            }
+        }else{
+            ctx.response.status = 400;
+        }
+    }else{
+        ctx.response.status = 404;
     }
 });
 
